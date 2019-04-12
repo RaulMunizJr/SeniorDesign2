8c17ae85d4ec2dc8293876c7596727108b6150ae
Raul Muniz <Raul.Muniz01@utrgv.edu>
2019-04-11 19:28:45 -0500

    Tool&clear1

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,19 @@
 class DashboardController < ApplicationController
 	def index
 	end
+
+	def tool1
+  		session[:load_part] = 1
+  		redirect_to dashboard_path
+ 	end
+
+ 	def tool2
+  		session[:load_part] = 2
+  		redirect_to dashboard_path
+ 	end
+
+ 	def nothing
+  		session[:load_part] = 3
+  		redirect_to dashboard_path
+ 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+	authenticated :user do
   get 'home/index'
   root 'home#index'
 
   get '/dashboard', to: "dashboard#index"
+
+  get :tool1, to: "dashboard#tool1"
+  get :tool2, to: "dashboard#tool2"
+  get :nothing, to: "dashboard#nothing"
+
+end
+	
+	unauthenticated do
+		root 'devise/session#new', as: :unauthenticated_root
+	end
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
